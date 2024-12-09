230b49ee08d8478f263c8bce9289546d76bc88ab
Gutavo <Koldin.araujo28@gmail.com>
2024-12-09 17:04:58 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+var setadireita = window.document.getElementById("setadireita")
+var setaesquerda = window.document.getElementById("setaesquerda")
+var taekwondo1 = window.document.getElementById("taekwondo1")
+var taekwondo4 = window.document.getElementById("taekwondo4")
+
+function DeslizarParaDireita() {
+    taekwondo1.style="display:nome"
+    taekwondo4.style="display:flex"
+    setadireita.style="display:none"
+    setaesquerda.style="dislay:flex, margin-top=50px" 
+}
+
+function DeslizarParaDireita() {
+    taekwondo1.style="display:flex"
+    taekwondo4.style="display:none"
+    setaesquerda.style="display:flex; margin-top=50px"
+    setadireita.style="display:none"
+}
+
+function DeslizarParaEsquerda() {
+    taekwondo3.style="display:nome"
+    taekwondo2.style="display:flex"
+    setaesquerda.style="display:none"
+    setadireita.style="dislay:flex, margin-top=50px" 
+}
+
+function DeslizarParaEsquerda() {
+    taekwondo3.style="display:flex"
+    taekwondo2.style="display:none"
+    setadireita.style="display:flex; margin-top=50px"
+    setaesquerda.style="display:none"
+}
